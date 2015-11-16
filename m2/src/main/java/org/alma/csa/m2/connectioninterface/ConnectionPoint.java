5b2ae93c949725907aa2f521bcd8de3546518f84
maximepvrt <maximepvrt@gmail.com>
2015-11-16 15:15:57 +0100

    🐣 add connection interface architecture + gitignore

---
+++ m2/src/main/java/org/alma/csa/m2/connectioninterface/ConnectionPoint.java
@@ -0,0 +1,7 @@
+package org.alma.csa.m2.connectioninterface;
+
+/**
+ * Created by Maxime on 16/11/2015.
+ */
+public class ConnectionPoint extends ConnectionInterface {
+}
---
+++ m2/src/main/java/org/alma/csa/m2/connectioninterface/SyncronizationType.java
@@ -0,0 +1,8 @@
+package org.alma.csa.m2.connectioninterface;
+
+/**
+ * Created by Maxime on 16/11/2015.
+ */
+public enum SyncronizationType {
+    
+}
